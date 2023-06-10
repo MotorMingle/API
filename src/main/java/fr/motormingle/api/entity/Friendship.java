2c56ca46d581afa9867c89cb9165247bd1bfd2f8
Lebarbanchon Valentin <valentin.lebarbanchon@ecole.ensicaen.fr>
2023-06-10 13:02:29 +0100

    7-create-base-api-classes - Added positions and users csv + db.inserts - Added immatriculation to Ownership - Created Friendship Entity - Encounter has now a status per user - Update db.changelog-master.yaml and db.init-schema.yaml

--- src/main/java/fr/motormingle/api/entity/Friendship.java
+++ src/main/java/fr/motormingle/api/entity/Friendship.java
@@ -3,7 +3,6 @@ package fr.motormingle.api.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,11 +10,11 @@ import java.time.LocalDate;
 
 @Getter
 @Setter
+@Table(name = "friendship")
 @Entity
-@Table(name = "encounter")
-public class Encounter {
+public class Friendship {
     @EmbeddedId
-    private EncounterId id;
+    private FriendshipId id;
 
     @MapsId("userId1")
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
@@ -29,31 +28,10 @@ public class Encounter {
     @NotNull
     private User userId2;
 
-    /**
-     * Hash of the H3 hexagonal cell identifier of the encounter.
-     */
-    @Column(name = "hash", nullable = false)
-    @NotNull
-    private String hash;
-
-    /**
-     * Number of encounters between the two users.
-     */
-    @Column(name = "count", nullable = false)
-    @NotNull
-    @Size(min = 1, max = 255)
-    private int count;
-
-    /**
-     * Last date of the encounter.
-     */
-    @Column(name = "date", nullable = false)
+    @Column(name = "date")
     @NotNull
     private LocalDate date;
 
-    /**
-     * The status of the encounter enumerated by the {@code EncounterStatus} enum.
-     */
     @Column(name = "user_1_status", nullable = false)
     @Enumerated(EnumType.STRING)
     @NotNull
@@ -65,5 +43,4 @@ public class Encounter {
     @NotNull
     @Pattern(regexp = "ACCEPTED|DECLINED|PENDING")
     private EncounterStatus user2Status;
-
 }
