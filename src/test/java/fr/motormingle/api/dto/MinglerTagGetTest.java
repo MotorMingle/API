c2fc3711cef61103bc3d966da4befb4d05b7a35d
EniXoo <lebarbanchon.valentin@gmail.com>
2023-08-03 20:03:35 +0100

    10-api-security-and-authention-setup ## Unit tests  - Upgrade coverage MinglerTagGet @EqualsAndHashCode

--- src/test/java/fr/motormingle/api/dto/MinglerTagGetTest.java
+++ src/test/java/fr/motormingle/api/dto/MinglerTagGetTest.java
@@ -21,6 +21,11 @@ class MinglerTagGetTest {
     }
 
     @Test
+    void testTagNull() {
+        assertThrows(NullPointerException.class, () -> minglerTagGet.setTag(null));
+    }
+
+    @Test
     void testRequiredArgsConstructor() {
         String tag = "enixo";
         MinglerTagGet minglerTagGet = new MinglerTagGet(tag);
@@ -29,16 +34,21 @@ class MinglerTagGetTest {
     }
 
     @Test
-    void testEqualsAndHashCode() {
+    void testEqualsAndHashCodeOk() {
         MinglerTagGet minglerTagGet1 = new MinglerTagGet("enixo");
         MinglerTagGet minglerTagGet2 = new MinglerTagGet("enixo");
+        assertEquals(minglerTagGet1, minglerTagGet1);
         assertEquals(minglerTagGet1, minglerTagGet2);
         assertEquals(minglerTagGet1.hashCode(), minglerTagGet2.hashCode());
     }
 
     @Test
-    void testTagNull() {
-        assertThrows(NullPointerException.class, () -> minglerTagGet.setTag(null));
+    void testEqualsAndHashCodeNotOk() {
+        MinglerTagGet minglerTagGet1 = new MinglerTagGet("enixo");
+        MinglerTagGet minglerTagGet2 = new MinglerTagGet("vixi9");
+        assertNotEquals(minglerTagGet1, new Object());
+        assertNotEquals(minglerTagGet1, minglerTagGet2);
+        assertNotEquals(minglerTagGet1.hashCode(), minglerTagGet2.hashCode());
     }
 
     @Test
